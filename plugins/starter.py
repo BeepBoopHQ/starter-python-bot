1444031a30ab0e07147a1fdc79a5e87888d8565c
Randall Barnhart <randall.barnhart@robotsandpencils.com>
2016-01-20 15:36:18 -0700

    Implemented pybot attachment command, through api_call.

--- plugins/starter.py
+++ plugins/starter.py
@@ -4,6 +4,7 @@ import random
 import logging
 crontable = []
 outputs = []
+attachments = []
 typing_sleep = 0
 
 greetings = ['Hi friend!', 'Hello there.', 'Howdy!', 'Wazzzup!!!', 'Hi!', 'Hey.']
@@ -23,21 +24,39 @@ p_bot_help = re.compile("pybot[\s]*help")
 
 def process_message(data):
     logging.debug("process_message:data: {}".format(data))
+
     if p_bot_hi.match(data['text']):
         outputs.append([data['channel'], "{}".format(random.choice(greetings))])
+
     elif p_bot_joke.match(data['text']):
         outputs.append([data['channel'], "Why did the python cross the road?"])
         outputs.append([data['channel'], "__typing__", 5])
         outputs.append([data['channel'], "To eat the chicken on the other side! :laughing:"])
+
     elif p_bot_attach.match(data['text']):
-        outputs.append([data['channel'], "Sorry, I can't do this yet. :disappointed:"])
+        txt = "Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots."
+        attachments.append([data['channel'], txt, build_demo_attachment(txt)])
+
     elif p_bot_help.match(data['text']):
         outputs.append([data['channel'], "{}".format(help_text)])
+
     elif data['text'].startswith("pybot"):
         outputs.append([data['channel'], "I'm sorry, I don't know how to: `{}`".format(data['text'])])
+
     elif data['channel'].startswith("D"):  # direct message channel to the bot
         outputs.append([data['channel'], "Hello, I'm the BeepBoop python starter bot.\n{}".format(help_text)])
 
 def process_mention(data):
     logging.debug("process_mention:data: {}".format(data))
     outputs.append([data['channel'], "You really do care about me. :heart:"])
+
+def build_demo_attachment(txt):
+    return {
+        "pretext" : "We bring bots to life. :sunglasses: :thumbsup:",
+		"title" : "Host, deploy and share your bot in seconds.",
+		"title_link" : "https://beepboophq.com/",
+		"text" : txt,
+		"fallback" : txt,
+		"image_url" : "https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png",
+		"color" : "#7CD197",
+    }
